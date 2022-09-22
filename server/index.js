63ae436387cb488a1c160497c534f00f61c95aee
Dahan Jo <dan0748@naver.com>
2022-09-22 23:48:32 +0900

    2022_0922 MongoDB commit

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require('cors')
+require('dotenv').config()
+
 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
