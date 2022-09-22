8361eed42cc7d6200c57e24478ae36e4c3793509
Dahan Jo <dan0748@naver.com>
2022-09-22 23:46:34 +0900

    2022_0922 MongoDB commit

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser");
 
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect("mongodb+srv://joda:asd123@cluster0.exq1i.mongodb.net/shop?retryWrites=true&w=majority",
+const connect = mongoose.connect(process.env.MONGO_URI,
     {
         useNewUrlParser: true, 
         useUnifiedTopology: true 
